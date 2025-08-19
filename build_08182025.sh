d6a6d27ce804e8a5031aa405a3320633dbf149e9
Jared Keith Averitt <103066434+JaredKeithAveritt@users.noreply.github.com>
2025-08-19 09:31:12 -0600

    Update build_08182025.sh

--- build_08182025.sh
+++ build_08182025.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-set main_dir=${PWD##*/}
+main_dir=${PWD}
 envName=p311a-local
 envDir=${main_dir}/envs/${envName}
 packageDir=${envDir}-packages
+mkdir envs
 mkdir ${packageDir}
+cd envs
 
 exec > >(tee -i ${envName}.log) 2>&1
 
@@ -26,16 +28,32 @@ source ${envDir}/bin/activate
 #export LD_LIBRARY_PATH=
 
 #-------------------lammps executable----------------------#
-export lmp="${envDir}-packages/lammps-kokkos-mliap/build/lmp"
+export lmp="${envDir}-packages/lammps/build/lmp"
 
 EOL
 
 
 chmod +x ${envDir}-load.bash
 
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+# do not allow conda init!!!!
 
-conda create --name HIPNN-01 python=3.11
-conda activate HIPNN-01
+source ~/miniconda3/etc/profile.d/conda.sh
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
+sudo apt install g++
+sudo apt install gfortran
+sudo apt install cmake
+sudo apt update
+sudo apt install -y mpich libmpich-dev
+sudo apt install -y nvidia-cuda-toolkit
+sudo apt update
+sudo apt install -y ffmpeg pkg-config
+
+conda create --prefix $envDir python=3.11 -y
+conda activate $envDir
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 pip3 install matplotlib
 pip3 install h5py
@@ -47,22 +65,50 @@ pip3 install zarr
 pip3 install ase
 pip3 install RDkit
 pip3 install ipykernel
+pip3 install numpy
 
 echo " ## ############################################ ##"
 echo " ##                Compiling FEFF                ##"
 echo " ## ############################################ ##"
 
-
 git clone https://github.com/pyscal/pyscal3.git
 pushd pyscal3
 #git checkout tags/3.2.6
-push pyscal3/
 pip3 install -r requirements.txt
 pip3 install .
 python setup.py build
 popd
-check pyscal3
+#check pyscal3
 pip show pyscal3
+
+
+mkdir FEFF
+pushd ${packageDir}/PythonPath/FEFF
+tar -xzf feff85L.tgz
+pushd feff85L
+pushd MONO
+rm Compile
+
+cat > Compile <<'EOF'
+#!/bin/bash
+set -euo pipefail
+
+# settings
+F77=gfortran
+flags='-O2 -ffree-line-length-none -static -std=legacy -fmax-errors=5 -w'
+
+# compile
+$F77 $flags -o opconsat  opconsat_tot.f90
+$F77 $flags -o eps2exc   eps2exc_tot.f
+$F77 $flags -o feff85L   feff85L.f
+EOF
+
+chmod +x Compile
+./Compile
+
+popd
+popd
+popd
 
 echo "## ############################################ ##"
 echo "##     Installing Pyiron and its depdencies     ##"
@@ -71,15 +117,26 @@ echo "## ############################################ ##"
 pip3 install pyiron-atomistics
 pip3 install pyiron-base
 pip3 install pyiron
-pushd packages
-git clone https://github.com/lanl/hippynn.git
+
+
+cd $packageDir
+
+git clone https://github.com/JaredKeithAveritt/hippynn
 pushd hippynn
-#git checkout torch-load-weights_only-fix
-#pip install -e ./
-git fetch origin pull/163/head:pr-163
-git checkout pr-163
 pip install -e ./
 popd
+
+
+git clone https://github.com/JaredKeithAveritt/lammps-UQ-MLIAP-Interface/ lammps
+pushd lammps
+mkdir build
+pushd build
+
+. build_lammps_local.sh
+
+make -j 8
+make install-python
+
 
 python -m ipykernel install --user --name=HIPNN-01
 
